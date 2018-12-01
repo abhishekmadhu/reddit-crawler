a544f6f9e3613eaa7763068499507805e4e00f6d
abhishekmadhu <abhishek.prince.madhu@gmail.com>
2018-12-01 23:34:50 +0530

    Tried my first web-crawler. Now I can die half-satisfied...

---
+++ reddit_crawler/reddit_crawler/spiders/redditbot.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RedditbotSpider(scrapy.Spider):
+    name = 'redditbot'
+    allowed_domains = ['www.reddit.com/r/gameofthrones/']
+    start_urls = ['http://www.reddit.com/r/gameofthrones/']
+
+    def parse(self, response):
+        titles = response.css(".ILDoH::text").extract()
+        votes = response.css("._1rZYMD_4xY3gRcSS3p8ODO::text").extract()
+        # there are dual entries of each vote count on each page
+        # cleanup begins
+        for i in range(0, int(len(votes)/2)):
+            votes[i] = votes[2*i]
+
+
+        # times = response.css('time::attr(title)').extract()
+        # comments = response.css('.comments::text').extract()
+
+        for item in zip(titles, votes):
+            # create a dictionary to store the scraped info
+            scraped_info = {
+                'title': item[0],
+                'vote': item[1]
+                # 'created_at': item[2],
+                # 'comments': item[3],
+            }
+
+            # yield or give the scraped info to scrapy
+            yield scraped_info
